31d00d47664c0cdf4c733ef3ac22b0645417d918
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-13 22:23:22 +0545

    update example usage

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,12 @@
 #include "pair.hpp"
 
 int main() {
-    pair<const char*, int> p;
+    pair<std::string, int> p;
 
-    p.first = "number";
-    p.second = 69;
-    
-    std::cout << p.first << std::endl;
-    std::cout << p.second << std::endl;
+    p.first = "Rajesh Thapa";
+    p.second = 19;
+
+    std::cout << p.first << " is " << p.second << " years old." << std::endl;
 
     return 0;
 }
